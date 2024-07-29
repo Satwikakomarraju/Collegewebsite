54167ce8054ae6ee8483e6d68316784c6400b55c
Satwikakomarraju <141409616+Satwikakomarraju@users.noreply.github.com>
2024-07-29 18:26:33 +0530

    Add files via upload

---
+++ website/1.js
@@ -0,0 +1,8 @@
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your form submission logic here
+    console.log('Form submitted!');
+});
